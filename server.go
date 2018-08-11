a7fd17ea7b5ee8a9e9f1161d266b299b47955b98
4423 <hanamiya4423@gmail.com>
2018-08-11 15:05:31 +0900

    initial commit

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func fizzbuzz(n int) string {
+	switch {
+	case n%3 == 0 && n%5 == 0:
+		return "Fizz Buzz"
+	case n%3 == 0:
+		return "Fizz"
+	case n%5 == 0:
+		return "Buzz"
+	default:
+		return strconv.Itoa(n)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	s := strings.Split(r.URL.Path, "/")
+	n, err := strconv.Atoi(s[len(s)-1])
+	if err != nil {
+		fmt.Fprintf(w, "invalid number")
+	} else {
+		fmt.Fprintf(w, fizzbuzz(n))
+	}
+}
+
+func main() {
+	http.HandleFunc("/fizzbuzz/", handler)
+	http.ListenAndServe(":8080", nil)
+}
